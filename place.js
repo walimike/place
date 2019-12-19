70399e68c2220d2bc0a4aaa37e9d7bb9585f4f44
Michael Wali <walimike139@gmail.com>
2019-12-19 21:07:44 +0300

    place login interface

---
+++ place.js
@@ -0,0 +1,6 @@
+// let email = document.getElementById("email-adress")
+// let password = document.getElementById("user-password")
+
+const loginMessage = () =>{
+    console.log("There was an error while logging in")
+}
